e24703728efeda4d63a9dc07dca13998a15a68c6
Suhan Wijaya <suhanw@gmail.com>
2022-03-01 13:42:24 -0500

    disable IE11

--- babel.config.js
+++ babel.config.js
@@ -13,7 +13,8 @@ module.exports = function (api) {
             {
                 corejs: 3, // To polyfill ES6 features for browsers that don't support them
                 useBuiltIns: 'usage', // With this, @babel/preset-env replaces direct imports of core-js to imports of only the specific modules required for a target environment: https://karen-kua.medium.com/the-end-of-babel-polyfill-the-more-efficient-alternative-95f959fef9c2
-                targets: 'last 2 versions, not dead, > 0.2%, not ie <= 10',
+                targets: 'defaults, not ie <= 11', // https://github.com/browserslist/browserslist#full-list
+                // targets: 'defaults',
                 // debug: true,
             },
         ],
--- src/utils/cookie/index.js
+++ src/utils/cookie/index.js
@@ -2,14 +2,17 @@ export const writeCookie = function ({ name, value, days = 730 /** 2 years */, d
     let date = new Date();
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     let expires = `; expires=${date.toUTCString()}`;
+    // let expires = '; expires=' + date.toUTCString();
     let cookieValue = `${name}=${value}${expires}; path=${path}`;
+    // let cookieValue = name + '=' + value + expires + '; path=' + path;
     if (domain) {
         cookieValue += `; domain=${domain}`;
+        // cookieValue += '; domain=' + domain;
     }
 
-	if (typeof document !== 'undefined') {
-		document.cookie = cookieValue;
-	}
+    if (typeof document !== 'undefined') {
+        document.cookie = cookieValue;
+    }
 };
 
 export const readCookie = function (name) {
