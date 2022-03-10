caa72ce3c92e7b127239680e3522ef1801a30d40
Suhan Wijaya <suhanw@gmail.com>
2022-03-10 15:14:20 -0500

    publish 0.1.0-alpha

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     entry: './src/index.js',
 
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'spresso.sdk.web.js',
         library: {
             name: 'SpressoSdk',
