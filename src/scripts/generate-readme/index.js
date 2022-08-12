c2938fd7270a312b736db3d2e94def273bbdb99e
Suhan Wijaya <suhanw@gmail.com>
2022-08-12 12:49:44 -0400

    publish docs

--- src/scripts/generate-readme/index.js
+++ src/scripts/generate-readme/index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { readFileSync, writeFileSync } = require('fs');
+const { readFileSync, writeFileSync, existsSync, mkdirSync } = require('fs');
 const ejs = require('ejs');
 
 const template = readFileSync(path.resolve(__dirname, './README.md.template'), 'utf-8');
@@ -7,5 +7,12 @@ const version = require(path.resolve(process.cwd(), 'package.json')).version;
 
 const newContent = ejs.render(template, { version }, {});
 
-writeFileSync(path.resolve(process.cwd(), 'docs/README.md'), newContent);
-console.log(`README.md updated with SDK version ${version}.`); 
+const readmePath = path.resolve(process.cwd(), 'docs');
+
+if (!existsSync(readmePath)) {
+    console.log(`making dir: ${readmePath}`);
+    mkdirSync(readmePath);
+}
+
+writeFileSync(`${readmePath}/README.md`, newContent);
+console.log(`README.md updated with SDK version ${version}.`);
