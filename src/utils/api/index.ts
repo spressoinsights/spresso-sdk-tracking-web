5d3d6a174d7296d94983de1061d5d1ce6284bae4
Suhan Wijaya <suhanw@gmail.com>
2022-08-15 13:30:11 -0400

    SPRS-384 TypeScript

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,25 +1,35 @@
 import 'cross-fetch/polyfill';
+import { IEventObject } from 'event-factory';
 import { isBrowser } from 'utils/browser';
 import { consoleLog } from 'utils/debug';
 
 const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
-export const track = function ({ orgId, events, useStaging }) {
+interface ITrackOptions {
+    orgId: string;
+    events: Array<IEventObject>;
+    useStaging: boolean;
+}
+
+export function track({ orgId, events, useStaging }: ITrackOptions) {
     consoleLog({ useStaging });
 
-    const ENDPOINT = useStaging ? 'https://public-pensieve-stats.us-east4.staging.spresso.com/track' : 'https://public-pensieve-stats.us-east4.prod.spresso.com/track';
+    let ENDPOINT = useStaging
+        ? 'https://public-pensieve-stats.us-east4.staging.spresso.com/track'
+        : 'https://public-pensieve-stats.us-east4.prod.spresso.com/track';
+
+    if (isDev) {
+        ENDPOINT = 'http://localhost:1337/track';
+    }
 
     if (!isBrowser() || !events?.length) {
         return;
     }
 
-    let body = {
-        datas: events,
-    };
-
     try {
-        consoleLog('FIRE', JSON.stringify(body, null, 2));
-        body = JSON.stringify(body);
+        consoleLog('FIRE', JSON.stringify({ datas: events }, null, 2));
+
+        const body = JSON.stringify({ datas: events });
 
         fetch?.(ENDPOINT, {
             method: 'POST',
@@ -38,4 +48,4 @@ export const track = function ({ orgId, events, useStaging }) {
     } catch (error) {
         console.error(error);
     }
-};
+}
